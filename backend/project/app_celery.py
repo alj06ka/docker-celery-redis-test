f9781e50e7ac8273e634cc711beccf30b4812ceb
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2019-05-22 10:03:25 +0300

    Remove google app client

--- backend/project/app_celery.py
+++ backend/project/app_celery.py
@@ -5,11 +5,6 @@ from google.cloud import storage
 
 # If you don't specify credentials when constructing the client, the
 # client library will look for credentials in the environment.
-storage_client = storage.Client()
-
-# Make an authenticated API request
-buckets = list(storage_client.list_buckets())
-print(buckets)
 
 app = Celery('app_celery', broker='redis://10.0.0.8:6379/0')
 
